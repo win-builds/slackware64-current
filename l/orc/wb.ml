7b01dfca13b83b48e661bc43fff912aa7aa7ce0b
Adrien Nader <adrien@notk.org>
2015-05-18 22:04:37 +0200

    orc: update to 0.4.23.

--- l/orc/wb.ml
+++ l/orc/wb.ml
@@ -1,10 +1,10 @@
     let orc = add (name, variant)
       ~dir
       ~dependencies:[]
-      ~version:"0.4.19"
+      ~version:"0.4.23"
       ~build:1
       ~sources:[
-        Tarball ("${PACKAGE}-${VERSION}.tar.gz", "6186a6a5faefe6b61d55e5406c7365d69b91c982");
+        Tarball ("${PACKAGE}-${VERSION}.tar.xz", "17283a871cc4bda968dc4d1e8022c6fbf8672d4b");
       ]
     in
 
