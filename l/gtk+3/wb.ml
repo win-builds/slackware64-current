2f4b5bc25e5811788b852b29b3649de97f418c2c
Adrien Nader <adrien@notk.org>
2015-05-18 22:24:05 +0200

    gtk+3: update to newer version to avoid an autoreconf that fails.
    Autoreconf failed in some quite mysterious way and new version of GTK+3
    don't have the issue so simply upgrade. However it cross-compiles an
    executable that it intended to run on the build and therefore links with
    the wrong flags compared to compilation. Fortunately it doesn't try to
    run the executable so we can just fool make by touching the build
    outputs.

--- l/gtk+3/wb.ml
+++ l/gtk+3/wb.ml
@@ -1,11 +1,10 @@
     let gtk_3 = add (name, variant)
       ~dir
       ~dependencies:[ pango; atk; cairo; glib2 ]
-      ~version:"3.12.2"
+      ~version:"3.14.13"
       ~build:1
       ~sources:[
-        Tarball ("gtk+-${VERSION}.tar.xz", "9727843d1389306fcad80f92bb50201f1f43f894");
-        Patch "correctly-generate-def-files-again.patch";
+        Tarball ("gtk+-${VERSION}.tar.xz", "422f344d01b6ab143db61a39f0e0a3bc3ae5337e");
       ]
     in
 
